95498660e20f678022cd46efb61d1cdeb6bb7365
Asim Aftab <asim.dev.cs@gmail.com>
2024-12-03 22:23:53 +0530

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var textView:TextView
+        textView=findViewById(R.id.viewTextView);
+        var clickBtn:Button = findViewById(R.id.clickId)
+        var inputBox:EditText=findViewById(R.id.inputEditText)
+        clickBtn.setOnClickListener { v->
+            inputBox.setText("Hello")
+        }
+    }
+}
